21d0a6a63f06f2039d9b0aa3551ed10dc261babb
Tatsiana Skorabahataya <skorobogataya.tatiana@gmail.com>
2024-03-28 19:56:12 +0300

    adjust units for decisions coverage

--- src/main/java/UnitDecisions.java
+++ src/main/java/UnitDecisions.java
@@ -3,28 +3,36 @@ public class UnitDecisions {
 
         MyInitClass myInitClass = new MyInitClass();
 
-        //check decision 1 in condition 1 : if condition returns true
-        int actual1 = myInitClass.getNumber(-60, 9);
-        if (actual1 == -20) {
+        //decision point 1 true, decision point 2 true
+        int actual1 = myInitClass.getNumber(-6, 8);
+        if (actual1 == 41) {
             System.out.println("Test one passed");
         } else {
             System.out.println("Test one failed");
         }
 
-        //check decision 2 in condition 1 : if condition returns false
-        int actual2 = myInitClass.getNumber(-200, 10);
-        if (actual2 == -280) {
+        //decision point 1 false, decision point 2 true
+        int actual2 = myInitClass.getNumber(6, 29);
+        if (actual2 == 88) {
             System.out.println("Test two passed");
         } else {
             System.out.println("Test two failed");
         }
 
-        //check decision in condition 2 : if condition returns true
-        int actual3 = myInitClass.getNumber(-6, 8);
-        if (actual3 == 41) {
+        //decision point one true, decision point 2 false
+        int actual3 = myInitClass.getNumber(-60, 9);
+        if (actual3 == -20) {
             System.out.println("Test three passed");
         } else {
             System.out.println("Test three failed");
+        }
+
+        //decision point one false, decision point 2 false
+        int actual4 = myInitClass.getNumber(-200, 10);
+        if (actual4 == -280) {
+            System.out.println("Test four passed");
+        } else {
+            System.out.println("Test four failed");
         }
     }
 }
