d33da97d44c2733d98e60bc6f659eb459b777ef9
Tatsiana Skorabahataya <skorobogataya.tatiana@gmail.com>
2024-03-29 21:50:32 +0300

    adjust units for conditions and decisions coverage

--- src/main/java/UnitConditions.java
+++ src/main/java/UnitConditions.java
@@ -2,36 +2,20 @@ public class UnitConditions {
     public static void main(String[] args) {
         MyInitClass myInitClass = new MyInitClass();
 
-        //condition 1 true
-        int actual1 = myInitClass.getNumber(-6, 8);
-        if (actual1 == 41) {
+        //condition 1: (false + true), condition 2: (true + false)
+        int actual1 = myInitClass.getNumber(10, -200);
+        if (actual1 == -135) {
             System.out.println("Test one passed");
         } else {
             System.out.println("Test one failed");
         }
 
-        //condition 1 false
-        int actual2 = myInitClass.getNumber(-200, 10);
-        if (actual2 == -280) {
+        //condition 1: (true + false), condition 2: (false + true)
+        int actual2 = myInitClass.getNumber(-10, 10);
+        if (actual2 == 45) {
             System.out.println("Test two passed");
         } else {
             System.out.println("Test two failed");
-        }
-
-        //condition 2 true
-        int actual3 = myInitClass.getNumber(6, 29);
-        if (actual3 == 88) {
-            System.out.println("Test three passed");
-        } else {
-            System.out.println("Test three failed");
-        }
-
-        //condition 2 false
-        int actual4 = myInitClass.getNumber(-60, 9);
-        if (actual4 == -20) {
-            System.out.println("Test four passed");
-        } else {
-            System.out.println("Test four failed");
         }
     }
 }
--- src/main/java/UnitDecisions.java
+++ src/main/java/UnitDecisions.java
@@ -3,7 +3,7 @@ public class UnitDecisions {
 
         MyInitClass myInitClass = new MyInitClass();
 
-        //decision point 1 true
+        //decision point 1 true, decision point 2 true
         int actual1 = myInitClass.getNumber(-6, 8);
         if (actual1 == 41) {
             System.out.println("Test one passed");
@@ -11,28 +11,12 @@ public class UnitDecisions {
             System.out.println("Test one failed");
         }
 
-        //decision point 1 false
+        //decision point 1 false, decision point 2 false
         int actual2 = myInitClass.getNumber(-200, 10);
         if (actual2 == -280) {
             System.out.println("Test two passed");
         } else {
             System.out.println("Test two failed");
-        }
-
-        //decision point 2 true
-        int actual3 = myInitClass.getNumber(6, 29);
-        if (actual3 == 88) {
-            System.out.println("Test three passed");
-        } else {
-            System.out.println("Test three failed");
-        }
-
-        //decision point 2 false
-        int actual4 = myInitClass.getNumber(-60, 9);
-        if (actual4 == -20) {
-            System.out.println("Test four passed");
-        } else {
-            System.out.println("Test four failed");
         }
     }
 }
