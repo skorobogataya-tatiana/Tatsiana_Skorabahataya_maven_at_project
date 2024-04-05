9741e0374b5ab3df467c3dff185d7f78badf4826
Tatsiana Skorabahataya <skorobogataya.tatiana@gmail.com>
2024-04-05 18:11:47 +0300

    add test suits with Junit and testNG fremeworks

---
+++ src/main/java/people/Person.java
@@ -0,0 +1,13 @@
+package people;
+
+public class Person {
+    private int age;
+
+    public Person(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
