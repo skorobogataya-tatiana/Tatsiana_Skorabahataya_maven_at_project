5d83dcd77fede453b30a9eae791625bbb20f1b86
Tatsiana Skorabahataya <skorobogataya.tatiana@gmail.com>
2024-04-10 18:26:06 +0300

    add class with navigate methods for web driver

---
+++ src/test/java/webdriver/MyWebDriverFirst.java
@@ -0,0 +1,14 @@
+package webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyWebDriverFirst {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+        driver.navigate().to("https://ya.ru/");
+        driver.navigate().back();
+        driver.navigate().refresh();
+    }
+}
