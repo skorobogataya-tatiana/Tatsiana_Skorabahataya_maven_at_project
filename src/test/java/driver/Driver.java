2dd0e4cb923eb559f5d470582bf467dadb6448df
Tatsiana Skorabahataya <skorobogataya.tatiana@gmail.com>
2024-04-26 20:18:16 +0300

    move methods from utils to driver class

--- src/test/java/driver/Driver.java
+++ src/test/java/driver/Driver.java
@@ -1,11 +1,19 @@
 package driver;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Optional;
 
@@ -27,7 +35,7 @@ public class Driver {
     private static WebDriver getChromeDriver() {
         if (null == driver) {
             ChromeOptions caps = new ChromeOptions();
-            caps.addArguments("start-maximized");
+            //caps.addArguments("start-maximized");
             caps.addArguments("disable-infobars");
             caps.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
             driver = new ChromeDriver(caps);
@@ -46,7 +54,29 @@ public class Driver {
     }
 
     public static void killDriver() {
-        driver.quit();
-        driver = null;
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+
+    public static void makeScreenshot() {
+        byte[] asBytes = ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
+
+        try {
+            Files.write(Paths.get("C:\\Users\\Tatsiana_Skorabahata\\Desktop\\automation\\JavaCourse\\hometasks\\hotels\\hotel.png"), asBytes);
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
+    }
+
+    public static void switchToTheLastBrowserTab() {
+        getWebDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+        new WebDriverWait(driver, Duration.ofSeconds(30))
+                .until(ExpectedConditions.numberOfWindowsToBe(2));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+        ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(tabs.size() - 1));
     }
 }
